bc7bcc3cdfab75e19722ace24cd46eca3bcec9ba
Nataniell-c <nataniel-carvalho@outlook.com>
2025-08-27 17:53:43 -0300

    Inicio do Projeto minimal - api.

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,7 +1,46 @@
 
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _ConfiguracaoAppSettings;
+    public DbContexto(IConfiguration ConfiguracaoAppSettings)
+    {
+        _ConfiguracaoAppSettings = ConfiguracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    public DbSet<Veiculo> Veiculos { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Email = "administrador@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var StringConexao = _ConfiguracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if (!string.IsNullOrEmpty(StringConexao))
+
+            {
+                optionsBuilder.UseMySql(StringConexao, ServerVersion.AutoDetect(StringConexao));
+
+            }
+        }
+
+
+
+    }
 }
